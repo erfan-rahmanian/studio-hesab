5ac695c92d5c1e97a977edff2395badd492912d4
۱۳۳ rahmanian <133rahmanian@gmail.com>
2025-06-08 19:19:31 +0000

    بازم فونت اعمال نشده

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
+        body: ['Vazirmatn', 'sans-serif'],
         headline: ['Alegreya', 'serif'],
         code: ['monospace'],
       },
